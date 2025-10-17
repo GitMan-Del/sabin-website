6441b48c20d6fd413ead08256243571753187380
unknown <github0921@gmail.com>
2025-10-17 15:26:31 +0300

    Test/1 , Optimizations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  experimental: {
+    // @ts-expect-error - not yet typed in Next.js 15
+    browsersListForSwc: true
+  }
+} satisfies NextConfig;
 
 export default nextConfig;
